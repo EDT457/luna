2737dba18c9891b1ebca92bf94c18e15ac55b31e
EDT457 <tan.ethan.d@gmail.com>
2024-05-29 12:25:48 -0700

    slash beta

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const config = {
     type: Phaser.AUTO,
     width: 800,
-    height: 600,
+    height: 800,
     physics: {
         default: 'arcade',
         arcade: {
@@ -13,7 +13,8 @@ const config = {
         preload: preload,
         create: create,
         update: update
-    }
+    },
+    pixelArt: true
 };
 
 const game = new Phaser.Game(config);
@@ -38,6 +39,7 @@ function create() {
     platforms.create(400, 568, 'platform').refreshBody();
 
     player = this.physics.add.sprite(400, 400, 'walk0');
+    player.setTexture('walk0', null, { scaleMode: Phaser.ScaleModes.NEAREST });
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
     player.setScale(3); 
@@ -75,7 +77,22 @@ function create() {
     cursors = this.input.keyboard.createCursorKeys();
     keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    slash = this.add.graphics();
+    drawSlash();
+    slash.setVisible(false);
+}
+
+function drawSlash() {
+    slash.clear();
+    slash.fillStyle(0x808080, 1);
+    slash.beginPath();
+    slash.arc(0, 0, 50, Phaser.Math.DegToRad(270), Phaser.Math.DegToRad(450), false);
+    slash.arc(0, 0, 40, Phaser.Math.DegToRad(450), Phaser.Math.DegToRad(270), true);
+    slash.closePath();
+    slash.fillPath();
 }
 
 function update() {
@@ -92,7 +109,25 @@ function update() {
         player.anims.play('turn');
     }
 
-    if ((cursors.up.isDown || keySpace.isDown) && player.body.touching.down) {
+    if ((cursors.up.isDown || keyW.isDown) && player.body.touching.down) {
         player.setVelocityY(-330);
+    }
+
+    if (keySpace.isDown) {
+        slash.setVisible(true);
+        slash.setPosition(player.x, player.y);
+        slash.setScale(2); // Adjust the scale as needed
+
+        // Animate the slash to glide like a real slash
+        this.tweens.add({
+            targets: slash,
+            x: player.x + 100, // Adjust the distance as needed
+            alpha: 0,
+            duration: 200, // Duration of the animation
+            onComplete: () => {
+                slash.setVisible(false);
+                slash.alpha = 1; // Reset alpha for next use
+            }
+        });
     }
 }
